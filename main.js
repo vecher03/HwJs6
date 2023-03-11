51ed96a99979c20bf63d674dc03091ab404a12e1
Ogohi <phoenix.cod4@mail.ru>
2023-03-11 14:16:42 +0500

    save

---
+++ main.js
@@ -0,0 +1,15 @@
+
+
+let one = +prompt('Кол-во ступенек');
+let two = prompt('Символ отступов');
+let three = prompt('Конечный символ');
+let four = '';
+let five;
+
+for (let i = 0; i < one; i++) {
+    four += two;
+    five = four + three;
+    console.log(res);
+} if (amount == 0) {
+    console.log(three);
+}
